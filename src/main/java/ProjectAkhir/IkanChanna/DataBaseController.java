c5d8acd792a1f72cb67cb504a806a18040ba6a4b
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2022-01-05 18:57:48 +0700

    progresdua

--- src/main/java/ProjectAkhir/IkanChanna/DataBaseController.java
+++ src/main/java/ProjectAkhir/IkanChanna/DataBaseController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  *
@@ -17,19 +25,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/channa")
 public class DataBaseController {
 
-    @GetMapping("/channa")
-    @ResponseBody
-    public List<Channa> getChanna(){
-        
-        List<Channa> channa = new ArrayList<>();
-        
-        ChannaJpaController controller = new ChannaJpaController();
-        
-        try{
-            channa = controller.findChannaEntities();
-        } catch (Exception e) {}
-        
-        return channa;
+    @Autowired
+    RipositoryIkanChanna repositoryIkanChanna;
+            
+    @PostMapping(value = "/addchanna")
+    public Channa addchanna(@RequestBody Channa Param)
+    {
+        repositoryIkanChanna.save(Param);
+        return Param;
+    }
+    
+    @GetMapping(value = "/allchanna")
+    public List<Channa> allchanna()
+    {
+        return repositoryIkanChanna.findAll();
+    }
+    
+    @PutMapping(value = "/updatechanna")
+    public Channa updatechanna(@RequestBody Channa Param)
+    {
+       return repositoryIkanChanna.save(Param);
+    }
+    
+    @DeleteMapping(value = "/hapuschanna")
+    public List<Channa> hapuschanna(@RequestParam int id)
+    {
+        repositoryIkanChanna.deleteById(id);
+        List<Channa> channalist = repositoryIkanChanna.findAll();
+        return channalist;
     }
 }
+    
+//    @GetMapping("/channa")
+//    @ResponseBody
+//    public List<Channa> getChanna(){
+//        
+//        List<Channa> channa = new ArrayList<>();
+//        
+//        ChannaJpaController controller = new ChannaJpaController();
+//        
+//        try{
+//            channa = controller.findChannaEntities();
+//        } catch (Exception e) {}
+//        
+//        return channa;
+//    }
+//}
 
